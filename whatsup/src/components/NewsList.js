21cdde3012388a6bd4e33e6795c964c325a8d895
roxi <you@example.com>
2022-12-30 11:04:51 +0100

    add filter buttons

--- whatsup/src/components/NewsList.js
+++ whatsup/src/components/NewsList.js
@@ -2,43 +2,64 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 
+const getArticles = async (query) => {
+	const res = await axios.get(
+		`https://newsapi.org/v2/${query}&apiKey=ed07a8d765b6434ca92bc832025d8c49`
+	);
+	return res.data.articles;
+};
 
 function NewsList() {
 	const [articles, setArticles] = useState([]);
 	const [searchKey, setSearchKey] = useState("");
+	const [query, setQuery] = useState(
+		"top-headlines?country=de"
+	);
 
 	useEffect(() => {
 		// TODO: hide API key
-		const getArticles = async (q) => {
-            q = encodeURIComponent(q);
-			const res = await axios.get(
-				`https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=ed07a8d765b6434ca92bc832025d8c49`
-			);
-			setArticles(res.data.articles);
-            console.log(q)
-		};
-		getArticles();
-        console.log(articles)
-        
-	}, []);
+		getArticles(query).then(setArticles);
+		console.log(articles);
+	}, [query]);
 
 	return (
 		<div>
+			<div>
+				<button
+					onClick={() =>
+						setQuery(
+							"top-headlines?sources=bbc-news"
+						)
+					}>
+					BBC
+				</button>
+				<button
+					onClick={() =>
+						setQuery("everything?language=ro")
+					}>
+					RO
+				</button>
+			</div>
 			<div className='searchbar'>
-                <input
+				<input
 					type='text'
-                    value={searchKey}
+					value={searchKey}
 					placeholder='Enter a search word'
-                    onChange={(e) => setSearchKey(e.target.value)} />
+					onChange={(e) =>
+						setSearchKey(e.target.value)
+					}
+				/>
 			</div>
 			<div className='article-list'>
 				{articles
 					.filter(({ description }) => {
-						return searchKey ?
-							description
+						return searchKey
+							? description
 									.toLowerCase()
 									.includes(
-										(searchKey||"").toLowerCase()
+										(
+											searchKey || ""
+										).toLowerCase()
 									)
 							: true;
 					})
