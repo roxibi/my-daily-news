0f7c1b6c2a829b3b99445d80505bfad08654922a
Roxana Bita <roxana.bita@gmail.com>
2023-01-05 22:30:43 +0100

    add default picture

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./styles/articles.css";
-import {
-	BrowserRouter as Router,
-} from "react-router-dom";
+import NewsImage from "../assets/news-img.png";
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 function NewsItem({ title, description, url, urlToImage, source, date }) {
 	return (
 		<Router>
 			<div className='article'>
-				
-				<img src={urlToImage} alt='imageofnews' />
+				<img src={urlToImage ? urlToImage : NewsImage} alt='imageofnews' />
 
 				<h3>
 					<a href={url}> {title} </a>
@@ -17,15 +16,13 @@ function NewsItem({ title, description, url, urlToImage, source, date }) {
 
 				<p>{description}</p>
 
-				<button
-					className='btn-grad'
-					id='card-button'>
+				<button className='btn-grad' id='card-button'>
 					{" "}
 					<a href={url} target='_blank'>
 						read
 					</a>
 				</button>
-				<div className="card-footer"></div>
+				<div className='card-footer'></div>
 				<p>{source}</p>
 				<p>{date.substring(11, 16)}</p>
 			</div>
